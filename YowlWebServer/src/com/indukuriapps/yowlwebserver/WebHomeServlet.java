e88dad7174ed870e6f2720415aced4be8927243a
Vishnu S A V Indukuri <vishnui@gmail.com>
2013-06-05 17:17:03 -0400

    Initial Commit

---
+++ YowlWebServer/src/com/indukuriapps/yowlwebserver/WebHomeServlet.java
@@ -0,0 +1,16 @@
+package com.indukuriapps.yowlwebserver;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebHomeServlet extends HttpServlet{
+	private static final long serialVersionUID = 8937170522548469106L;
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)	throws IOException {
+		
+	}
+}
